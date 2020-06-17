0396d797fef8ec6b933fe9d257bab466979b7d9b
niuxinyu <1096196462@qq.com>
2020-06-17 17:25:36 +0800

    spec-key getBySpuId

--- src/main/java/io/github/talelin/latticy/controller/v1/SpecKeyController.java
+++ src/main/java/io/github/talelin/latticy/controller/v1/SpecKeyController.java
@@ -1,8 +1,7 @@
 package io.github.talelin.latticy.controller.v1;
 
 
-import io.github.talelin.latticy.service.SkuService;
-import io.github.talelin.latticy.service.SpuService;
+import io.github.talelin.latticy.service.SpecKeyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import io.github.talelin.latticy.model.SkuDO;
+import io.github.talelin.latticy.model.SpecKeyDO;
 import io.github.talelin.latticy.vo.CreatedVO;
 import io.github.talelin.latticy.vo.DeletedVO;
 import io.github.talelin.latticy.vo.PageResponseVO;
@@ -27,13 +26,13 @@ import java.util.List;
 
 /**
 * @author generator@TaleLin
-* @since 2020-06-16
+* @since 2020-06-17
 */
 @RestController
-@RequestMapping("/v1/sku")
-public class SkuController {
+@RequestMapping("/v1/spec-key")
+public class SpecKeyController {
     @Autowired
-    private SkuService skuService;
+    private SpecKeyService specKeyService;
 
     @PostMapping("")
     public CreatedVO create() {
@@ -51,17 +50,17 @@ public class SkuController {
     }
 
     @GetMapping("/{id}")
-    public SkuDO get(@PathVariable(value = "id") @Positive(message = "{id.positive}") Long id) {
+    public SpecKeyDO get(@PathVariable(value = "id") @Positive(message = "{id.positive}") Long id) {
         return null;
     }
 
     @GetMapping("/by/spu/{id}")
-    public List<SkuDO> getBySpuId(@PathVariable(value = "id") @Positive Long spuId){
-        return this.skuService.lambdaQuery().eq(SkuDO::getSpuId, spuId).list();
+    public List<SpecKeyDO> getBySpuId(@PathVariable(value = "id") @Positive Long spuId){
+        return this.specKeyService.getBySpuId(spuId);
     }
 
     @GetMapping("/page")
-    public PageResponseVO<SkuDO> page(
+    public PageResponseVO<SpecKeyDO> page(
             @RequestParam(name = "count", required = false, defaultValue = "10")
             @Min(value = 1, message = "{page.count.min}")
             @Max(value = 30, message = "{page.count.max}") Long count,
---
+++ src/main/java/io/github/talelin/latticy/mapper/SpecKeyMapper.java
@@ -0,0 +1,18 @@
+package io.github.talelin.latticy.mapper;
+
+import io.github.talelin.latticy.model.SpecKeyDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author generator@TaleLin
+ * @since 2020-06-17
+ */
+public interface SpecKeyMapper extends BaseMapper<SpecKeyDO> {
+    List<SpecKeyDO> getBySpuId(Long spuId);
+}
